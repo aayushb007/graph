19be0b45a59dba2862b61ae490cfa0b1f46fef64
aayushb007 <66074436+aayushb007@users.noreply.github.com>
2020-05-29 14:25:39 +0530

    Add files via upload

---
+++ densityplot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import seaborn as sns
+data = [90, 80, 50, 42, 89, 78, 34, 70, 67, 73, 74, 80, 60, 90, 90]
+sns.distplot(data)
+
+
+# In[2]:
+
+
+sns.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
